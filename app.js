e291a497563e1d560f50455860d52eaefea34688
githubdmn <dmninbox@gmail.com>
2021-03-20 17:19:40 +0100

    items route removed

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ express()
 	.use(express.urlencoded({ extended: true }))
 	.get('/', (req, res) => { res.status(200).send(`Home - item-0321 app ${Date.now()}`) })
 	.get('/health', (req, res) => { res.status(200).send('OK') })
-	.use('/api', app)
+	//.use('/api', app)
 	.listen(PORT, () => { console.log(`Listening on port ${PORT} ...`); });
 
 
