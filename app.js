e0465d576f9cea4aa0df7384a1de2fee05d611c7
githubdmn <dmninbox@gmail.com>
2021-03-20 14:15:47 +0100

    Update

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const { PORT } = process.env;
 express()
 	.use(express.json())
 	.use(express.urlencoded({ extended: true }))
-	.get('/', (req, res) => { res.status(200).send('Home - item-0321 app')})
-	.get('/health', (req, res) => { res.status(200).send('OK')})
-	.listen(PORT, () => { console.log(`Listening on port ${PORT}...`);});
+	.get('/', (req, res) => { res.status(200).send('Home - item-0321 app 14:15 20') })
+	.get('/health', (req, res) => { res.status(200).send('OK') })
+	.listen(PORT, () => { console.log(`Listening on port ${PORT} ...`); });
 
 
 
