fc7c9a95bd48c2705b5eeae367223efe1bb90162
githubdmn <dmninbox@gmail.com>
2021-03-20 17:23:35 +0100

    items route added

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ require('dotenv').config({
 const { PORT } = process.env;
 
 
-const app = require('./router-items');
+const app = require('./router-items/itemsRouter');
 
 express()
 	.use(express.json())
 	.use(express.urlencoded({ extended: true }))
 	.get('/', (req, res) => { res.status(200).send(`Home - item-0321 app ${Date.now()}`) })
 	.get('/health', (req, res) => { res.status(200).send('OK') })
-	//.use('/api', app)
+	.use('/api', app)
 	.listen(PORT, () => { console.log(`Listening on port ${PORT} ...`); });
 
 
