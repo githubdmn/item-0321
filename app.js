2d3bdf1fbdf58d27282d1f5d00ab5460de67ed3d
githubdmn <dmninbox@gmail.com>
2021-03-21 14:18:57 +0100

    Sun 21 Mar 14:18:00 CET 2021

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = require('./router-items/items-router');
 express()
 	.use(express.json())
 	.use(express.urlencoded({ extended: true }))
-	.get('/', (req, res) => { res.status(200).send(`Home - item-0321 app staging- 21`) })
+	.get('/', (req, res) => { res.status(200).send(`Home - Sun 21 Mar 14:18:00 CET 2021`) })
 	.get('/health', (req, res) => { res.status(200).send('OK') })
 	.use('/api', app)
 	.listen(PORT, () => { console.log(`Listening on port ${PORT} ...`); });
