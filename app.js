ce6edae8bcd0b2efee99cfe3cca02645ed7fc4a7
githubdmn <dmninbox@gmail.com>
2021-03-25 12:01:02 +0100

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = require('./router-items/items-router');
 express()
 	.use(express.json())
 	.use(express.urlencoded({ extended: true }))
-	.get('/', (req, res) => { res.status(200).send(`Home - Wed 24 Mar 11:19:22 CET 2021`) })
+	.get('/', (req, res) => { res.status(200).send(`Home Thu 25 Mar 12:00:09 CET 2021`) })
 	.get('/health', (req, res) => { res.status(200).send('OK') })
 	.use('/api', app)
 	.listen(PORT, () => { console.log(`Listening on port ${PORT} ...`); });
